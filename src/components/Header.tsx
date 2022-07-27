05073fc4a52654743025dd3e380b873d1f0c98cf
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-07-27 16:55:06 -0300

    Add header and pages

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import aragonLogo from '../../public/images/aragonLogo.svg';
+
+export function Header() {
+  return (
+    <div className="bg-white py-6 px-11 flex flex-col items-center justify-between lg:flex-row">
+      <Link href="/">
+        <button className="flex items-center">
+          <Image src={aragonLogo} alt={'Aragon'} />
+          <h3 className="font-bold text-transparent bg-clip-text bg-gradient-to-t from-purple-50 to-purple-200 ml-6">
+            TAO VOTING
+          </h3>
+        </button>
+      </Link>
+      <div className="flex flex-col items-center font-semibold mt-2 md:flex-row lg:mt-0">
+        <Link href="/about">
+          <button className="text-gray my-6 md:mr-10">
+            What is Tao Voting?
+          </button>
+        </Link>
+        <Link href="/configuration">
+          <button className="bg-blue hover:bg-blue-200 text-white py-3 px-10 rounded-lg drop-shadow-sm transition ease-in-out duration-500">
+            Go to Configuration
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
