40972cb9263689f102743bb8233cba516fce9425
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-07-29 15:51:23 -0300

    add framer motion animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
-import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
-import { HomeHero } from '../components/Home/Hero';
-import { Notification } from '../components/Home/Notification';
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import { HomeHero } from '../components/Home/Hero'
+import { Notification } from '../components/Home/Notification'
+import { PageAnimation } from '../components/PageAnimation'
 
 export default function Home() {
   return (
-    <div className="bg-gray-0 bg-home bg-no-repeat bg-cover flex flex-col justify-between min-h-screen">
+    <div className="bg-gray-50 bg-home bg-no-repeat bg-cover flex flex-col justify-between min-h-screen">
       <Header />
       <div className="relative flex-grow">
         <Notification />
-        <HomeHero />
+        <PageAnimation>
+          <HomeHero />
+        </PageAnimation>
       </div>
       <Footer />
     </div>
-  );
+  )
 }
