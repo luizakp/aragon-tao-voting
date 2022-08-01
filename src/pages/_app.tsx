49fc70ea9582701025e437eb4fd9ed614f6c672d
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-08-01 18:26:42 -0300

    wip: configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,27 @@
 import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ParamsProvider } from '../hooks/useParams'
+import { TaoVotingProvider } from '../hooks/useTaoVoting'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Aragon Tao Voting</title>
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-      <AnimatePresence
-        exitBeforeEnter
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </>
+    <ParamsProvider>
+      <TaoVotingProvider>
+        <Head>
+          <title>Aragon Tao Voting</title>
+          <link rel="icon" href="/favicon.svg" />
+        </Head>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </TaoVotingProvider>
+    </ParamsProvider>
   )
 }
 
