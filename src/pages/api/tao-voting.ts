db4ce764ffa018a887cdccf9bd0f8a35f8536c48
Vitor <marthendalnunes@gmail.com>
2022-08-01 11:43:45 -0300

    refactor: change proposalInfo schema

--- src/pages/api/tao-voting.ts
+++ src/pages/api/tao-voting.ts
@@ -1,5 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { TaoVoting } from '../../models/taoVoting'
+import { ITaoVoting, IDisputableVoting } from '../../types/taoVoting'
+
+interface ITaoVotingPostData {
+  taoVoting: ITaoVoting
+  disputableVoting: IDisputableVoting
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,7 +14,7 @@ export default async function handler(
   if (req.method != 'POST') {
     res.status(405).end(`Method ${req.method} Not Allowed`)
   }
-  const data = req.body
+  const data: ITaoVotingPostData = req.body
   const taoVoting = new TaoVoting(data.taoVoting, data.disputableVoting)
   const response = await taoVoting.getData()
   res.status(200).json({ data: response })
