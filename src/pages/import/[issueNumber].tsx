22c6f20375daa1ca1c6bb688ab1b698013bbb14d
luizapuccinelli <luiza.puccinelli06@gmail.com>
2022-08-08 11:16:18 -0300

    fix unsed import

--- src/pages/import/[issueNumber].tsx
+++ src/pages/import/[issueNumber].tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import classnames from 'classnames'
